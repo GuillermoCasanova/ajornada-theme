b7dd2e57b6a4a9e1182864d02798346f691b246a
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2023-08-18 14:56:41 -0400

    + Styling buy near me modal for desktop
     + Adding . scroll to section for when clicking the buy near me button

--- assets/modal-buy-near-me.js
+++ assets/modal-buy-near-me.js
@@ -1,6 +1,23 @@
 class ModalBuyNearMe extends GlobalModal {
     constructor() {
       super();
+      this.modal = document.querySelector("#buy-near-me-modal");
+    }
+
+    openModal(pEvent) {
+      super.openModal(pEvent); 
+      console.log('hello');
+      this.scrollToSection(); 
+    }
+
+    scrollToSection() {
+      const offset = 150; // Replace with your desired offset value
+      const targetElement = this.modal;
+      const targetPosition =
+          targetElement.getBoundingClientRect().top +
+          window.pageYOffset -
+          offset;
+        window.scrollTo({ top: targetPosition, behavior: 'smooth' });
     }
   }
 
