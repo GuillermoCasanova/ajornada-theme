3f6d24a32c5388a102f81e4907bd2a22fadb1d1a
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2024-08-18 17:06:25 -0400

    + Adding feature so when faq questions are clicked they open the faq question on the faq list compoent

--- assets/faq-list.js
+++ assets/faq-list.js
@@ -1,78 +1,68 @@
-
 class FAQList extends HTMLElement {
-    constructor() {
-      super();
-      this.activeDrawer = null;
-      console.log('hello');
-      if (Shopify.designMode) {
-        console.log('design mode');
-        document.addEventListener('shopify:block:select', this.onBlockSelect.bind(this));
-      }
-    }
-  
-    connectedCallback() {
-      this.init();
-    }
-  
- 
-    toggleDrawer(event) {
-      event.preventDefault();
-      const currentTarget = event.currentTarget;
-      this.openDrawer(currentTarget);
-    }
+  constructor() {
+    super();
+    this.activeDrawer = null;
+  }
 
-    closeDrawer(pElem) {
-      this.activeDrawer = null;
-      const faqAnswer = pElem.querySelector('[data-faq-answer]');
-      faqAnswer.style.height = 0;
-      pElem.querySelector('summary').setAttribute('aria-expanded', false);
-  
-      setTimeout(() => {
-        pElem.removeAttribute('open');
-      }, 450);
-    }
-  
-    onBlockSelect(pEvent) {
-      console.log(pEvent);
-    }
-
-    openDrawer(pDrawer) {
-      pDrawer.setAttribute('aria-expanded', true);
-    
-      if (pDrawer.dataset.id == this.activeDrawer) {
-        this.closeDrawer(pDrawer.closest('details'));
-        return;
-      }
-  
-      this.querySelectorAll('details').forEach((elem) => {
-        if (elem.querySelector('summary').dataset.id !== pDrawer.dataset.id) {
-          this.closeDrawer(elem.closest('details'));
-        }
-      });
-  
-      pDrawer.closest('details').setAttribute('open', true);
-  
-      const faqAnswer = pDrawer.closest('details').querySelector('[data-faq-answer]');
-      faqAnswer.style.height = faqAnswer.querySelector('p').offsetHeight + 'px';
-  
-      this.activeDrawer = pDrawer.dataset.id;
-    }
-  
-    init() {
-      this.querySelectorAll('details').forEach((elem) => {
-        const summary = elem.querySelector('summary');
-            summary.addEventListener('click', (e) => {
-             this.toggleDrawer(e);
-            });
-
-        elem
-          .querySelectorAll('[data-faq-answer]')
-          .forEach((elem) => {
-            elem.style.height = 0;
-          });
-      });
+  connectedCallback() {
+    this.init();
+    if (Shopify.designMode) {
+      document.addEventListener('shopify:block:select', this.onFAQBlockSelect.bind(this));
     }
   }
-  
-  // Define the custom element
-  customElements.define('faq-list', FAQList);
+
+  toggleDrawer(event) {
+    event.preventDefault();
+    this.openDrawer(event.currentTarget);
+  }
+
+  closeDrawer(pElem) {
+    if (!pElem) return;
+
+    pElem.querySelector('[data-faq-answer]').style.height = 0;
+    pElem.querySelector('summary').setAttribute('aria-expanded', false);
+
+    setTimeout(() => {
+      pElem.removeAttribute('open');
+    }, 450);
+
+    this.activeDrawer = null;
+  }
+
+  onFAQBlockSelect(event) {
+    event.preventDefault();
+    setTimeout(() => this.openDrawer(event.target), 200);
+  }
+
+  openDrawer(pDrawer) {
+    const parentDetails = pDrawer.closest('details');
+
+    if (parentDetails && parentDetails.dataset.id === this.activeDrawer) {
+      this.closeDrawer(parentDetails);
+      return;
+    }
+
+    this.querySelectorAll('details').forEach(elem => {
+      if (elem !== parentDetails) this.closeDrawer(elem);
+    });
+
+    if (parentDetails) {
+      parentDetails.setAttribute('open', true);
+      const faqAnswer = parentDetails.querySelector('[data-faq-answer]');
+      faqAnswer.style.height = `${faqAnswer.querySelector('p').offsetHeight}px`;
+      pDrawer.setAttribute('aria-expanded', true);
+      this.activeDrawer = parentDetails.dataset.id;
+    }
+  }
+
+  init() {
+    this.querySelectorAll('details').forEach(detailsElem => {
+      detailsElem.querySelector('summary').addEventListener('click', this.toggleDrawer.bind(this));
+      detailsElem.querySelectorAll('[data-faq-answer]').forEach(answerElem => {
+        answerElem.style.height = 0;
+      });
+    });
+  }
+}
+
+customElements.define('faq-list', FAQList);
